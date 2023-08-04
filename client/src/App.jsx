54b9a6056728c7cff5da96e292ffb15edd87cbdb
Eren Tasci <erent4sci@gmail.com>
2023-08-04 17:26:48 +0300

    feat: statistics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
 import BillPage from "./pages/BillPage";
 import CartPage from "./pages/CartPage";
 import CustomerPage from "./pages/CustomerPage";
 import HomePage from "./pages/HomePage";
+import StatisticPage from "./pages/StatisticPage";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Route path="/cart" element={<CartPage />} />
         <Route path="/bills" element={<BillPage />} />
         <Route path="/customers" element={<CustomerPage />} />
+        <Route path="/statistic" element={<StatisticPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
