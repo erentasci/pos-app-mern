b775ee1b76ee2844deca6efb0a9fb3ac8a694b32
Eren Tasci <erent4sci@gmail.com>
2023-08-04 17:29:02 +0300

    rev

--- client/src/pages/StatisticPage.jsx
+++ client/src/pages/StatisticPage.jsx
@@ -23,27 +23,27 @@ const StatisticPage = () => {
 
   const data2 = [
     {
-      type: "分类一",
+      type: '分类一',
       value: 27,
     },
     {
-      type: "分类二",
+      type: '分类二',
       value: 25,
     },
     {
-      type: "分类三",
+      type: '分类三',
       value: 18,
     },
     {
-      type: "分类四",
+      type: '分类四',
       value: 15,
     },
     {
-      type: "分类五",
+      type: '分类五',
       value: 10,
     },
     {
-      type: "其他",
+      type: '其他',
       value: 5,
     },
   ];
