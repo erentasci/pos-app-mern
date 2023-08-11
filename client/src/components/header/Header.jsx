b91c12f76e6b4f3a56131f1464eda33f98a8f42a
Eren Tasci <erent4sci@gmail.com>
2023-08-11 19:13:28 +0300

    rev: header logos

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -30,45 +30,39 @@ const Header = () => {
         <div className="menu-links flex justify-between items-center gap-7 md:static fixed z-50 bottom-0 md:w-auto w-screen md:bg-transparent bg-white left-0 md:border-t-0 border-t md:px-0 px-4 py-1">
           <Link
             to={"/"}
-            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
-          >
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all justify-center items-center">
             <HomeOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Ana Sayfa</span>
           </Link>
           <Badge count={5} offset={[0, 6]} className="md:flex hidden">
             <Link
               to={"/cart"}
-              className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
-            >
+              className="menu-link flex flex-col hover:text-[#40a9ff] transition-all justify-center items-center">
               <ShoppingCartOutlined className="md:text-2xl text-xl" />
               <span className="md:text-xs text-[10px]">Sepet</span>
             </Link>
           </Badge>
           <Link
             to={"/bills"}
-            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
-          >
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all justify-center items-center">
             <CopyOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Faturalar</span>
           </Link>
           <Link
             to={"/customers"}
-            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
-          >
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all justify-center items-center">
             <UserOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Müşteriler</span>
           </Link>
           <Link
             to={"/statistic"}
-            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
-          >
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all justify-center items-center">
             <BarChartOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">İstatistikler</span>
           </Link>
           <Link
             to={"/"}
-            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
-          >
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all justify-center items-center">
             <LogoutOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Çıkış</span>
           </Link>
@@ -76,8 +70,7 @@ const Header = () => {
         <Badge count={5} offset={[0, 6]} className="md:hidden flex">
           <Link
             to={"/"}
-            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
-          >
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all justify-center items-center">
             <ShoppingCartOutlined className="text-2xl" />
             <span className="md:text-xs text-[10px]">Sepet</span>
           </Link>
